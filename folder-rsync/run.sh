40780d41518671eaa29158530305b1387bf5a3cb
Carsten Schröder <carsten@schroeder.net>
2019-02-27 16:13:03 +0100

    hass function for folder rsync

--- folder-rsync/run.sh
+++ folder-rsync/run.sh
@@ -7,9 +7,10 @@ CONFIG_PATH=/data/options.json
 rsyncserver=$(jq --raw-output ".rsyncserver" $CONFIG_PATH)
 rootfolder=$(jq --raw-output ".rootfolder" $CONFIG_PATH)
 username=$(jq --raw-output ".username" $CONFIG_PATH)
-password=$(jq --raw-output ".password" $CONFIG_PATH)
+#password=$(jq --raw-output ".password" $CONFIG_PATH)
 
 rsyncurl="$username@$rsyncserver::$rootfolder"
+password=="$(hass.config.get 'password')"
 
 echo "[Info] trying to rsync hassio folders to $rsyncurl"
  sshpass -p $password rsync -av /config/ $rsyncurl/config/ 
