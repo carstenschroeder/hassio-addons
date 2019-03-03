acbb3a9b30c8b711d89974b3ae73a76a37b7762d
Carsten Schröder <carsten@schroeder.net>
2019-03-03 18:14:44 +0100

    decorate output

--- folder-rsync/run.sh
+++ folder-rsync/run.sh
@@ -12,9 +12,19 @@ password=$(jq --raw-output ".password" $CONFIG_PATH)
 rsyncurl="$username@$rsyncserver::$rootfolder"
 
 echo "[Info] trying to rsync hassio folders to $rsyncurl"
+echo ""
+echo "[Info] /config"
  sshpass -p $password rsync -av /config/ $rsyncurl/config/ 
+echo ""
+echo "[Info] /addons"
  sshpass -p $password rsync -av /addons/ $rsyncurl/addons/ 
+echo ""
+echo "[Info] /backup"
  sshpass -p $password rsync -av /backup/ $rsyncurl/backup/ 
+echo ""
+echo "[Info] /share"
  sshpass -p $password rsync -av /share/ $rsyncurl/share/ 
+echo ""
+echo "[Info] /ssl"
  sshpass -p $password rsync -av /ssl/ $rsyncurl/ssl/ 
 echo "[Info] Finished rsync"
