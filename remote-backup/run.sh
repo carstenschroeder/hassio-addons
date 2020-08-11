cbd675ce3ed610c32fd66d4c888725137bc4892b
Matheson Steplock <ikifar2012@users.noreply.github.com>
2020-08-10 21:46:55 -0400

    SSH Keys and other updates

--- remote-backup/run.sh
+++ remote-backup/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 CONFIG_PATH=/data/options.json
 
 # parse inputs from options
@@ -8,7 +7,7 @@ SSH_ENABLED=$(jq --raw-output ".ssh_enabled" $CONFIG_PATH)
 SSH_HOST=$(jq --raw-output ".ssh_host" $CONFIG_PATH)
 SSH_PORT=$(jq --raw-output ".ssh_port" $CONFIG_PATH)
 SSH_USER=$(jq --raw-output ".ssh_user" $CONFIG_PATH)
-SSH_KEY=$(jq --raw-output ".ssh_key[]" $CONFIG_PATH)
+SSH_KEY=$(jq --raw-output ".ssh_key" $CONFIG_PATH)
 REMOTE_DIRECTORY=$(jq --raw-output ".remote_directory" $CONFIG_PATH)
 ZIP_PASSWORD=$(jq --raw-output '.zip_password' $CONFIG_PATH)
 KEEP_LOCAL_BACKUP=$(jq --raw-output '.keep_local_backup' $CONFIG_PATH)
@@ -20,13 +19,14 @@ RSYNC_USER=$(jq --raw-output ".rsync_user" $CONFIG_PATH)
 RSYNC_PASSWORD=$(jq --raw-output ".rsync_password" $CONFIG_PATH)
 
 # create variables
-SSH_ID="${HOME}/.ssh/id"
-
+SSH_ID="/ssl/${SSH_KEY}"
+SSH_ID=$(echo -n ${SSH_ID})
 function add-ssh-key {
 
     if [ "$SSH_ENABLED" = true ] ; then
         echo "Adding SSH key"
         mkdir -p ~/.ssh
+        cp ${SSH_ID} ${HOME}/.ssh/id
         (
             echo "Host remote"
             echo "    IdentityFile ${HOME}/.ssh/id"
@@ -36,9 +36,9 @@ function add-ssh-key {
             echo "    StrictHostKeyChecking no"
         ) > "${HOME}/.ssh/config"
 
-        while read -r line; do
-            echo "$line" >> ${HOME}/.ssh/id
-        done <<< "$SSH_KEY"
+        # while read -r line; do
+        #     echo "$line" >> ${HOME}/.ssh/id
+        # done <<< "$SSH_KEY"
 
         chmod 600 "${HOME}/.ssh/config"
         chmod 600 "${HOME}/.ssh/id"
@@ -62,22 +62,22 @@ function copy-backup-to-remote {
 
 function delete-local-backup {
 
-    hassio snapshots reload
+    ha snapshots reload
 
     if [[ ${KEEP_LOCAL_BACKUP} == "all" ]]; then
         :
     elif [[ -z ${KEEP_LOCAL_BACKUP} ]]; then
         echo "Deleting local backup: ${slug}"
-        hassio snapshots remove "${slug}"
+        ha snapshots remove "${slug}"
     else
 
-        last_date_to_keep=$(hassio snapshots list --raw-json | jq .data.snapshots[].date | sort -r | \
+        last_date_to_keep=$(ha snapshots list --raw-json | jq .data.snapshots[].date | sort -r | \
             head -n "${KEEP_LOCAL_BACKUP}" | tail -n 1 | xargs date -D "%Y-%m-%dT%T" +%s --date )
 
-        hassio snapshots list --raw-json | jq -c .data.snapshots[] | while read backup; do
+        ha snapshots list --raw-json | jq -c .data.snapshots[] | while read backup; do
             if [[ $(echo ${backup} | jq .date | xargs date -D "%Y-%m-%dT%T" +%s --date ) -lt ${last_date_to_keep} ]]; then
                 echo "Deleting local backup: $(echo ${backup} | jq -r .slug)"
-                hassio snapshots remove "$(echo ${backup} | jq -r .slug)"
+                ha snapshots remove "$(echo ${backup} | jq -r .slug)"
             fi
         done
 
@@ -87,7 +87,7 @@ function delete-local-backup {
 function create-local-backup {
     name="Automated backup $(date +'%Y-%m-%d %H:%M')"
     echo "Creating local backup: \"${name}\""
-    slug=$(hassio snapshots new --raw-json --name="${name}" | jq --raw-output '.data.slug')
+    slug=$(ha snapshots new --raw-json --name="${name}" | jq --raw-output '.data.slug')
     echo "Backup created: ${slug}"
 }
 
@@ -95,7 +95,7 @@ function rsync_folders {
 
     if [ "$RSYNC_ENABLED" = true ] ; then
         rsyncurl="$RSYNC_USER@$RSYNC_HOST::$RSYNC_ROOTFOLDER"
-        echo "[Info] trying to rsync hassio folders to $rsyncurl"
+        echo "[Info] trying to rsync ha folders to $rsyncurl"
         echo ""
         echo "[Info] /config"
          sshpass -p $RSYNC_PASSWORD rsync -av --exclude '*.db-shm' --exclude '*.db-wal' /config/ $rsyncurl/config/ --delete
