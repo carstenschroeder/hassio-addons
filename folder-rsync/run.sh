61ed67a3c3e09627b17d7edc1591f9382a8ea164
Viktor Brack <viktor.brack@tu-dortmund.de>
2021-02-28 01:45:38 +0100

    Add delete option to folder-rsync

--- folder-rsync/run.sh
+++ folder-rsync/run.sh
@@ -14,23 +14,23 @@ rsyncurl="$username@$rsyncserver::$rootfolder"
 echo "[Info] trying to rsync hassio folders to $rsyncurl"
 echo ""
 echo "[Info] /config"
-sshpass -p $password rsync -av --exclude '*.db-shm' --exclude '*.db-wal' /config/ $rsyncurl/config/ 
+sshpass -p $password rsync -av --exclude '*.db-shm' --exclude '*.db-wal' /config/ $rsyncurl/config/ --delete
 echo ""
 echo "[Info] /addons"
-sshpass -p $password rsync -av /addons/ $rsyncurl/addons/ 
+sshpass -p $password rsync -av /addons/ $rsyncurl/addons/ --delete
 echo ""
 echo "[Info] /backup"
-sshpass -p $password rsync -av /backup/ $rsyncurl/backup/ 
+sshpass -p $password rsync -av /backup/ $rsyncurl/backup/ --delete
 echo ""
 echo "[Info] /share"
-sshpass -p $password rsync -av /share/ $rsyncurl/share/ 
+sshpass -p $password rsync -av /share/ $rsyncurl/share/ --delete
 echo ""
 echo "[Info] /ssl"
-sshpass -p $password rsync -av /ssl/ $rsyncurl/ssl/ 
+sshpass -p $password rsync -av /ssl/ $rsyncurl/ssl/ --delete
 if [ -d "/media" ]; then
  echo ""
  echo "[Info] /media"
- sshpass -p $password rsync -av /media/ $rsyncurl/media/
+ sshpass -p $password rsync -av /media/ $rsyncurl/media/ --delete
 else 
  echo ""
  echo "[Info] /media not existing"
